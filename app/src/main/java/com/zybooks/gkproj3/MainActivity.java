35ca5998227c85995bb574e2ae9f668b8e1a4b8b
gkhramt738 <gkhramtchenko738@g.rwu.edu>
2023-01-05 15:55:56 -0500

    Initial commit

---
+++ app/src/main/java/com/zybooks/gkproj3/MainActivity.java
@@ -0,0 +1,37 @@
+package com.zybooks.gkproj3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                    R.id.navigation_home, R.id.navigation_seaweed_salad, R.id.navigation_salmon_sushi, R.id.navigation_mixed_sashimi)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+            NavigationUI.setupWithNavController(navView, navController);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/zybooks/gkproj3/SalmonSushiFragment.java
@@ -0,0 +1,19 @@
+package com.zybooks.gkproj3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class SalmonSushiFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_salmon_sushi, container, false);
+    }
+
+}
